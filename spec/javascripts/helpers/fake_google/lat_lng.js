61cccfea2e5b654fd70dad90dc5a09d4b289ad4b
Alissa Sobo & Max Brunsfeld <maxbrunsfeld+alissa+max@gmail.com>
2012-09-30 23:40:37 -0700

    Improve mock map and LatLng

--- spec/javascripts/helpers/fake_google/lat_lng.js
+++ spec/javascripts/helpers/fake_google/lat_lng.js
@@ -1,4 +1,17 @@
 google.maps.LatLng = function(lat, lng) {
-  this.lat = lat;
-  this.lng = lng;
+  this._lat = lat;
+  this._lng = lng;
 };
+
+_.extend(google.maps.LatLng.prototype, {
+
+  lat: function() {
+    return this._lat;
+  },
+
+  lng: function() {
+    return this._lng;
+  }
+
+});
+
--- spec/javascripts/helpers/fake_google/map.js
+++ spec/javascripts/helpers/fake_google/map.js
@@ -1,5 +1,14 @@
 google.maps.Map = function(el, options) {
   this.el = el;
   this.options = options;
+  this.center = options.center;
   google.backdoor.initalizeMap(this);
 };
+
+_.extend(google.maps.Map.prototype, {
+
+  setCenter: function(latLng) {
+    this.center = latLng;
+  }
+
+});
