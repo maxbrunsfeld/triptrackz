4553e5b32f28d1f8de2eb79698c05acd55bc376a
Alissa Sobo & Max Brunsfeld <maxbrunsfeld+alissa+max@gmail.com>
2012-10-22 23:44:43 -0700

    Remove console.logs

--- app/assets/javascripts/presenters/index.js
+++ app/assets/javascripts/presenters/index.js
@@ -5,11 +5,8 @@
 presenters = {
 
   present: function(templateName, data) {
-    console.log(data);
     var json = data.map(function(model) {
       return model.attributes;
-
-      console.log("attributes:", model.attributes);
     });
 
     return presenters[templateName](json);
